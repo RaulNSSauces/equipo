513f7a5d73b055f0d3b93312fd823492ee5d791d
Raul Nuñez Sebastian <raul.nunseb@educa.jcyl.es>
2020-11-16 09:25:13 +0100

    Commit README

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>EQUIPO6</title>
+    </head>
+    <body>
+        <h1>Página Web Desarrollador 1</h1>
+        <p><a href="nacho.php">Página de Nacho</a></p>
+    </body>
+</html>
+
