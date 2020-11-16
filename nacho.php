c1c983025c690a0a4fe11a0426681ac5bb645b9a
Nacho del Prado Losada <ignacio.pralos@educa.jcyl.es>
2020-11-16 09:32:32 +0100

    Add nacho.php

---
+++ nacho.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>EQUIPO 6</title>
+    </head>
+    
+    <body>
+        <h1>Proyecto Equipo 6</h1>
+        <p><a href="index.php">Página de Raúl</a></p>
+        
+    </body>
+    
+</html>
